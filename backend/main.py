1f4412d48410ccf27db2cae403fcd2b0553f2410
Pawan Jogi <pjpawam007@gmail.com>
2026-06-10 13:52:30 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://evo-ds-ai.vercel.app"
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+        "https://evo-ds-ai.vercel.app",
+        "https://evo-ds-appqzsw08-pawanjogi07s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
